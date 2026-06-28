452b3ccb5d230abbe398f3a245c615b927d5eb5d
Skimcher <zelenyilyg@gmail.com>
2026-06-28 18:32:42 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
 // Serve client files
-app.use(express.static(path.join(__dirname, '..', '..', 'client')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Game state
 const waitingPlayers = [];
